df0d4268df80eefcee71a48cd1d25657914941d1
Anton Kovalenko <CaribbeanLegend@mail.ru>
2021-02-24 11:18:30 +1000

    Получать салон отдельным запросом

--- src/models.ts
+++ src/models.ts
@@ -3,6 +3,24 @@ import Worker from '@/models/Worker';
 import ServiceType from '@/models/ServiceType';
 import { Config } from '@/config';
 
+function fetchData<T>(url: string): Promise<T | null> {
+  return fetch(Config.BACKEND_URL + url, {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json',
+    },
+  }).then(res => {
+    if (!res.ok) {
+      return null;
+    }
+
+    return res.json();
+  }).then((json: T) => {
+    return json;
+  });
+}
+
+
 function fetchDataList<T>(url: string): Promise<T[] | null> {
   return fetch(Config.BACKEND_URL + url, {
     method: 'GET',
@@ -31,27 +49,15 @@ export function getBeautyshops(city: string): Promise<Beautyshop[]> {
 }
 
 export function getBeautyshop(uuid: string): Promise<Beautyshop | null> {
-  return fetchDataList<Beautyshop>('/beautyshops?city=Москва').then((data: Beautyshop[] | null) => {
-    if (!data) {
-      return null;
-    }
-
-    // TODO Написать отдельное АПИ для получения салона по UUID
-
-    const beautyshop = data.find((beautyshop: Beautyshop) => {
-      if (beautyshop.uuid === uuid) {
-        return beautyshop;
-      }
-    });
-
+  return fetchData<Beautyshop>('/beautyshop?uuid=' + uuid).then((beautyshop: Beautyshop | null) => {
     return beautyshop ?? null;
   });
 }
 
 export function getWorkers(beautyShopUuid: string): Promise<Worker[] | null> {
-  return fetchDataList<Worker>('/workers?beautyshop=' + beautyShopUuid);
+  return fetchDataList<Worker>('/workers?beautyshopUuid=' + beautyShopUuid);
 }
 
 export function getServiceTypes(beautyShopUuid: string): Promise<ServiceType[] | null> {
-  return fetchDataList<ServiceType>('/service-types?beautyshop=' + beautyShopUuid);
+  return fetchDataList<ServiceType>('/service-types?beautyshopUuid=' + beautyShopUuid);
 }
