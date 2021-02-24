12dd7256e74949856691ec03474d8583c8f29e9c
Anton Kovalenko <CaribbeanLegend@mail.ru>
2021-02-24 11:18:13 +1000

    Выпилить меню

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
   <div class="app-container">
-    <div id="nav">
-      <router-link to="/hello">Hello</router-link> &#9658;
-      <router-link to="/list">List</router-link> &#9658;
-      <router-link to="/check-in">Check in</router-link> &#9658;
-      <router-link to="/info">Info</router-link> &verbar;
-      <router-link to="/api-test">API test</router-link>
-    </div>
-    <div class="content">
-      <router-view/>
-    </div>
+    <router-view/>
   </div>
 </template>
