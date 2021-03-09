3128102c2c6241b3573b36ba72f384cca8c6e66f
Anton Kovalenko <CaribbeanLegend@mail.ru>
2021-03-09 12:26:04 +1000

    При получении записи в салон красоты проверять актуальность

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,13 +1,18 @@
 import { GetterTree } from 'vuex';
 import { State } from './state';
 import CheckInItem from '@/models/CheckInItem';
+import dayjs from 'dayjs';
 
 export type Getters = {
   getCheckInItem(state: State): () => CheckInItem | null;
 }
 
 export const getters: GetterTree<State, State> & Getters = {
-  getCheckInItem: (state) => () => {
+  getCheckInItem: (state: State) => () => {
+    if (state.checkInItem?.startDate && dayjs(state.checkInItem.startDate).isBefore(dayjs())) {
+      return null;
+    }
+
     return state.checkInItem;
   },
 }
